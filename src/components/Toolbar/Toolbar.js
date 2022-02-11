d1e3852d9028a417411e27b716b1ba40870d455a
Eron Mahmuti <erooonmahmuti@gmail.com>
2022-02-11 16:33:02 +0100

    Toolbar to App

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Toolbar = props => {
+    <header>
+        <nav >
+            <div></div>
+            <div> <a href='/'> Logo </a> </div>
+            <div> 
+                    <ul>
+                        <li>
+                            <a href='/'> Products </a>
+                        </li>
+                        <li>
+                            <a href='/'> Users </a>
+                        </li>                        
+                    </ul>
+                </div>
+        </nav>
+    </header>
+}
+
+export default Toolbar;
